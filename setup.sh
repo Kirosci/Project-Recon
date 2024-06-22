67a5cc75396cf16062dfdaf65235feeefeb3447e
shivpratapsingh111 <shivpratapsingh777.2@gmail.com>
2024-06-22 23:41:33 +0530

    Update

--- setup.sh
+++ setup.sh
@@ -318,3 +318,17 @@ fi
 
 # Calling mainFunction
 mainFunction
+
+
+# Install docker in debian
+# sudo apt update
+# sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+# curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+# sudo apt update
+# sudo apt install docker-ce
+
+# Start Docker
+# sudo systemctl start docker
+# sudo systemctl enable docker
+# sudo systemctl status docker

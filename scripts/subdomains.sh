0e682645781711c2b9b40f0b3df36f09093298eb
shivpratapsingh111 <shivpratapsingh777.2@gmail.com>
2024-06-26 23:59:21 +0530

    Update

--- scripts/subdomains.sh
+++ scripts/subdomains.sh
@@ -202,7 +202,8 @@ activeEnumeration() {
     cat dnsgen.txt alterx.txt altdns.txt 2> /dev/null | sort -u > totalPermuted.txt
 
     # Puredns will resolve the permuted subdomains 
-    puredns resolve "totalPermuted.txt" -q > tee -a activeSubdomains.txt
+    puredns resolve "totalPermuted.txt" -q >> activeSubdomains.txt
+    sort -u activeSubdomains.txt -o activeSubdomains.txt
     print_message "$GREEN" "Active Enumeration Done] [Active Subdomains: $(cat 'activeSubdomains.txt' 2> /dev/null | wc -l)"                            
     cat activeSubdomains.txt passiveSubdomains.txt | sort -u > active+passive.txt
 
